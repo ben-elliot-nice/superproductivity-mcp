b7d362ddf7818765ad0f9d917017a6c0b6ea223a
Ben Elliot <ben.elliot@nice.com>
2026-04-24 12:57:44 +1000

    chore: bump version to v1.2.0

--- mcp_server.py
+++ mcp_server.py
@@ -1015,7 +1015,7 @@ class SuperProductivityMCPServer:
                 write_stream,
                 InitializationOptions(
                     server_name="super-productivity",
-                    server_version="1.1.0",
+                    server_version="1.2.0",
                     capabilities=self.server.get_capabilities(
                         notification_options=NotificationOptions(),
                         experimental_capabilities={},
